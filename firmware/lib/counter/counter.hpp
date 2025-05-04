87a1a2c90dd8645e697f8633f4c89812fba8979e
DaanSijnja <DaanSijnja@gmail.com>
2025-05-04 13:51:42 +0200

    created counter.hpp
    adds clock class used for counting up
    
    adds compare to use as an compare object for the timer

---
+++ firmware/lib/counter/counter.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+namespace counter
+{
+
+    typedef unsigned long long int counter_t;
+
+    class clock {
+        public:
+            constexpr clock() noexcept {}
+
+            auto get_count() -> counter_t {
+                return count;
+            }
+
+            auto up_count(const int value) -> void {
+                count += value;
+            }
+            
+        private:
+            counter_t count = 0;
+    };
+
+    class compare {
+         public:
+            compare(clock *c, const unsigned int interval) noexcept :
+            c(c),
+            interval(interval)
+            {}
+
+            auto compare_count(const bool reset = true) noexcept -> bool {
+                const counter_t value = c->get_count();
+                if( value - last_interval >= interval ) {
+                    if(reset){ last_interval = value; }
+                    return true;
+                }
+                return false;
+            }
+
+            auto set_interval(const unsigned int interval) noexcept -> void {
+                this->interval = interval;
+            }
+
+            auto set_last_interval(const counter_t last_interval) noexcept -> void {
+                 this->last_interval = last_interval;
+            }
+
+            auto set_last_interval_to_current() -> void {
+                 this->last_interval = c->get_count();
+            }
+
+        private:
+            clock *c;
+            unsigned int interval;
+            counter_t last_interval;
+    };
+} 
